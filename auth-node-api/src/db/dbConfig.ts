c96e6264c0c927e2ecc4a1e52cb5c9c59ee07b6d
Luis Felipe da Silva Mota de Souza <116108026+Lupesms97@users.noreply.github.com>
2024-03-20 16:07:55 -0300

    infra: db mongo configuration file

--- auth-node-api/src/db/dbConfig.ts
+++ auth-node-api/src/db/dbConfig.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose'
 
 
-const uri = "mongodb+srv://lupesms97:<FFUbyEC2M33XM2ce>@cluster0.z6fyrj1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://lupesms97:FFUbyEC2M33XM2ce@cluster0.chyk6kv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-export const createConnection = () => {
+export const createConnectionDB = () => {
     mongoose.Promise = global.Promise;
     mongoose.connect(uri)
         .then(() => console.log('Database connected'))
