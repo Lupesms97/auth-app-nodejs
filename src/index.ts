14933abf8a42f165f3b2c9e2a675214310704bd2
Luis Felipe da Silva Mota de Souza <116108026+Lupesms97@users.noreply.github.com>
2024-03-26 14:18:31 -0300

    update: changes ports

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3400;
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
